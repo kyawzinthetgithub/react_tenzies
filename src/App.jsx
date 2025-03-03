d651496dc7dcdfe2d51f06d49e30dba16794ee94
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-03 21:24:16 +0630

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "@/components/Main";
+
+export default function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center bg-[#0b2434] p-5">
+        <Main />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <>
+      <main className="bg-white w-full h-full md:max-w-[400px] md:max-h-[400px] rounded-xl">
+        <h1>main component</h1>
+      </main>
+    </>
+  );
+}
