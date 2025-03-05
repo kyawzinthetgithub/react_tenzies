3dcef1fc1307d6924ae2d85b3079ca9225a98204
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-06 00:14:23 +0630

    set timer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,21 +19,35 @@ export default function Main() {
    */
   const [dices, setDices] = useState(() => generateAllNewDice());
   const refBtn = useRef(null);
+  const [isStart, setIsStart] = useState(false);
+  const [time, setTime] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
   const gameWon =
     dices.every((dice) => dice.isHeld) &&
     dices.every((dice) => dice.value === dices[0].value);
 
+  const startGame = () => {
+    setIsStart(true);
+    setTime(0);
+    const id = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+    setIntervalId(id);
+  };
+
   const rollDice = () => {
-    gameWon
-      ? setDices(generateAllNewDice())
-      : setDices((oldDices) =>
-          oldDices.map((dice) =>
-            dice.isHeld
-              ? dice
-              : { ...dice, value: Math.ceil(Math.random() * 6) }
-          )
-        );
+    if (gameWon) {
+      setDices(generateAllNewDice());
+      setIsStart(false);
+      clearInterval(intervalId);
+    } else {
+      setDices((oldDices) =>
+        oldDices.map((dice) =>
+          dice.isHeld ? dice : { ...dice, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    }
   };
 
   const hold = (id) => {
@@ -45,8 +59,30 @@ export default function Main() {
   };
 
   useEffect(() => {
-    gameWon ? refBtn.current.focus() : null;
-  }, [gameWon]);
+    if (gameWon) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+      refBtn.current.focus();
+    }
+  }, [gameWon, intervalId]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return (
+      <div className="flex gap-x-5 items-center">
+        <p className="w-10 h-10 flex justify-center items-center border rounded-lg border-gray-300 text-[#4726fb]">
+          {minutes}
+        </p>
+        min
+        <p>:</p>
+        <p className="w-10 h-10 flex justify-center items-center border rounded-lg border-gray-300 text-[#18ce07]">
+          {seconds} 
+        </p>
+        sec
+      </div>
+    );
+  };
 
   return (
     <>
@@ -62,11 +98,20 @@ export default function Main() {
         </div>
         <div className="text-center">
           <h1 className="text-3xl font-bold">Tenzies</h1>
-          <p className="text-center my-5">
-            Roll until all dice are the same. Click each die to freeze it at its
-            current value between rolls.
+          <p className="text-center mt-2">
+            {gameWon ? (
+              <span>
+                Congratulation! You won! Press "New Game" to start again.
+              </span>
+            ) : (
+              <span>
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value between rolls.
+              </span>
+            )}
           </p>
         </div>
+        <div className="my-5 text-xl">{isStart && formatTime(time)}</div>
         <div className="die-container grid grid-cols-5 gap-5 w-full">
           {dices &&
             dices.map((dice) => (
@@ -78,14 +123,24 @@ export default function Main() {
               />
             ))}
         </div>
-        <button
-          ref={refBtn}
-          type="button"
-          onClick={rollDice}
-          className="cursor-pointer flex justify-center items-center rounded bg-[#4726fb] text-white hover:bg-[#a36ec7] transition-all px-6 py-1"
-        >
-          {gameWon ? "New Game" : "Roll"}
-        </button>
+        {isStart ? (
+          <button
+            ref={refBtn}
+            type="button"
+            onClick={rollDice}
+            className="cursor-pointer flex justify-center items-center rounded bg-[#4726fb] text-white hover:bg-[#a36ec7] transition-all px-6 py-1"
+          >
+            {gameWon ? "New Game" : "Roll"}
+          </button>
+        ) : (
+          <button
+            onClick={startGame}
+            type="button"
+            className="cursor-pointer flex justify-center items-center rounded bg-[#4726fb] text-white hover:bg-[#a36ec7] transition-all px-6 py-1"
+          >
+            Start
+          </button>
+        )}
       </main>
     </>
   );
